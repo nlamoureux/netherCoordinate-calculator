ade6d15a7be847805a2664763c2f43de42140da4
Nathan Lamoureux <144638257+nlamoureux@users.noreply.github.com>
2024-07-25 19:30:25 -0400

    Update App.java
    Commenting the program.

--- source/App.java
+++ source/App.java
@@ -1,3 +1,13 @@
+//Code by Nathan Lamoureux
+
+
+/*
+This program is a coordiunate calculator for the game Minecraft. It is meant to translate coordinates in the overworld dimension, to those
+ * of the nether dimension. It consists of two files: the main file that launches the application, and the file containing the construction of
+ * the window, as well as the function for coordinate conversion. The program takes whole numbers only, as decimals are not important in Minecraft.
+*/
+
+
 
 public class App {
     public static void main(String[] args) {
